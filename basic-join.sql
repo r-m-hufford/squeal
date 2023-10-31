f44f73d08c8df350146ee9ebee6503d79553556c
Ryan Hufford <ryan@rmhufford.com>
2023-10-31 09:12:31 -0400

    inner join + group by

--- basic-join.sql
+++ basic-join.sql
@@ -23,3 +23,21 @@ SELECT CITY.NAME FROM CITY
 LEFT JOIN COUNTRY
 ON CITY.COUNTRYCODE = COUNTRY.CODE
 WHERE COUNTRY.CONTINENT = 'Africa';
+
+/* Average Population of Each Continent
+Given the CITY and COUNTRY tables, query the name of the continents
+and their respective average city populations. Round the average population
+*/
+SELECT COUNTRY.CONTINENT, FLOOR(AVG(CITY.POPULATION)) FROM CITY
+INNER JOIN COUNTRY
+ON CITY.COUNTRYCODE = COUNTRY.CODE
+GROUP BY COUNTRY.CONTINENT;
+/* An inner Join is used to combine the records of the city and county
+table. The task is to combine records, not show records from one table and
+show the related records from another table. Therefore, an inner join is
+used. 
+The other thing going on is the GROUP BY. GROUP BY (on the non grouped fields)
+is required whenever aggregate functions are used. AVG aggregates all the 
+city.population values. Querying the country.continent aggregates nothing.
+The result is an ambigbuous query. GROUP BY eliminates this issue.
+*/
