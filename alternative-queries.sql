9c117498305b81dab1299cc290e8a940426efc62
Ryan Hufford <ryan@rmhufford.com>
2023-11-02 08:40:53 -0400

    triangle 2

--- alternative-queries.sql
+++ alternative-queries.sql
@@ -2,3 +2,10 @@
 SET @TEMP:=21; 
 SELECT REPEAT('* ', @TEMP:= @TEMP - 1) 
 FROM INFORMATION_SCHEMA.TABLES;
+
+/* Draw the Triangle 2 */
+SET @TEMP:=0;
+SELECT REPEAT('* ', @TEMP:= @TEMP + 1)
+FROM INFORMATION_SCHEMA.TABLES
+WHERE @TEMP < 21;
+/* SELECT prints things, WHERE filters things. Its that simple. */
